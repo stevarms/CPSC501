e2b59bd669370b5944a75c64a6c3435d57e26c85
Stephen Armstrong <stevarms@gmail.com>
2012-10-10 15:06:08 -0600

    refactoring complete, tests added for refactored code

--- src/SWN3.java
+++ src/SWN3.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
@@ -12,40 +14,13 @@ public class SWN3 {
 	public SWN3() {
 		System.out.println("Initializing...");
 		_dict = new HashMap<String, String>();
-		HashMap<String, Vector<Double>> _temp = new HashMap<String, Vector<Double>>();
 		try {
-			BufferedReader csv = new BufferedReader(new FileReader(pathToSWN));
-			String line = "";
-			while ((line = csv.readLine()) != null) {
-				String[] data = line.split("\t");
-				Double score = Double.parseDouble(data[2])
-						- Double.parseDouble(data[3]);
-				String[] words = data[4].split(" ");
-				for (String w : words) {
-					String[] w_n = w.split("#");
-					w_n[0] += "#" + data[0];
-					int index = Integer.parseInt(w_n[1]) - 1;
-					if (_temp.containsKey(w_n[0])) {
-						Vector<Double> v = _temp.get(w_n[0]);
-						if (index > v.size())
-							for (int i = v.size(); i < index; i++)
-								v.add(0.0);
-						v.add(index, score);
-						_temp.put(w_n[0], v);
-					} else {
-						Vector<Double> v = new Vector<Double>();
-						for (int i = 0; i < index; i++)
-							v.add(0.0);
-						v.add(index, score);
-						_temp.put(w_n[0], v);
-					}
-				}
-			}
-			Set<String> temp = _temp.keySet();
+			HashMap<String, Vector<Double>> rawDataMap = buildHashMap();
+			Set<String> temp = rawDataMap.keySet();
 			for (Iterator<String> iterator = temp.iterator(); iterator
 					.hasNext();) {
 				String word = (String) iterator.next();
-				Vector<Double> v = _temp.get(word);
+				Vector<Double> v = rawDataMap.get(word);
 				double score = 0.0;
 				double sum = 0.0;
 				for (int i = 0; i < v.size(); i++)
@@ -53,31 +28,69 @@ public class SWN3 {
 				for (int i = 1; i <= v.size(); i++)
 					sum += (double) 1 / (double) i;
 				score /= sum;
-				String sent = "";
-				if (score <= 1.0 && score >= 0.75)
-					sent = "very_strong_positive";
-				else if (score < 0.75 && score >= 0.5)
-					sent = "strong_positive";
-				else if (score < 0.5 && score >= 0.25)
-					sent = "positive";
-				else if (score < 0.25 && score > 0)
-					sent = "weak_positive";
-				else if (score == 0)
-					sent = "neutral";
-				else if (score < 0 && score >= -0.25)
-					sent = "weak_negative";
-				else if (score < -0.25 && score >= -0.5)
-					sent = "negative";
-				else if (score < -0.5 && score >= -0.75)
-					sent = "strong_negative";
-				else if (score < -0.5 && score >= -1)
-					sent = "very_strong_negative";
-				_dict.put(word, sent);
+				String result = getSentiment(score);
+				_dict.put(word, result);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		System.out.println("Initialization Complete");
+	}
+
+	String getSentiment(double score) {
+		String result = "";
+		if (score <= 1.0 && score >= 0.75)
+			result = "very_strong_positive";
+		else if (score < 0.75 && score >= 0.5)
+			result = "strong_positive";
+		else if (score < 0.5 && score >= 0.25)
+			result = "positive";
+		else if (score < 0.25 && score > 0)
+			result = "weak_positive";
+		else if (score == 0)
+			result = "neutral";
+		else if (score < 0 && score > -0.25)
+			result = "weak_negative";
+		else if (score <= -0.25 && score > -0.5)
+			result = "negative";
+		else if (score <= -0.5 && score > -0.75)
+			result = "strong_negative";
+		else if (score <= -0.75 && score >= -1)
+			result = "very_strong_negative";
+		return result;
+	}
+
+	private HashMap<String, Vector<Double>> buildHashMap()
+			throws FileNotFoundException, IOException {
+		HashMap<String, Vector<Double>> rawDataMap = new HashMap<String, Vector<Double>>();
+		BufferedReader csv = new BufferedReader(new FileReader(pathToSWN));
+		String line = "";
+		while ((line = csv.readLine()) != null) {
+			String[] data = line.split("\t");
+			Double score = Double.parseDouble(data[2])
+					- Double.parseDouble(data[3]);
+			String[] words = data[4].split(" ");
+			for (String word : words) {
+				String[] definition = word.split("#");
+				definition[0] += "#" + data[0];
+				int index = Integer.parseInt(definition[1]) - 1;
+				if (rawDataMap.containsKey(definition[0])) {
+					Vector<Double> scoreVector = rawDataMap.get(definition[0]);
+					if (index > scoreVector.size())
+						for (int i = scoreVector.size(); i < index; i++)
+							scoreVector.add(0.0);
+					scoreVector.add(index, score);
+					rawDataMap.put(definition[0], scoreVector);
+				} else {
+					Vector<Double> v = new Vector<Double>();
+					for (int i = 0; i < index; i++)
+						v.add(0.0);
+					v.add(index, score);
+					rawDataMap.put(definition[0], v);
+				}
+			}
+		}
+		return rawDataMap;
 	}
 
 	public void printAll(){
@@ -88,7 +101,7 @@ public class SWN3 {
 			System.out.println("Word: "+word+" Value: "+sent);
 		}
 	}
-	public String extract(String word, String pos) {
-		return _dict.get(word + "#" + pos);
+	public String extract(String word, String type) {
+		return _dict.get(word + "#" + type);
 	}
 }
--- src/SWN3Test.java
+++ src/SWN3Test.java
@@ -1,62 +1,182 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 
 public class SWN3Test {
 	
 	@Test
-	public void test_very_strong_positive() {
+	public void test_extract_very_strong_positive() {
 		SWN3 myswn = new SWN3();
 		String test = myswn.extract("perked_up","a");
 		assertTrue(test == "very_strong_positive");
 	}
 	@Test
-	public void test_strong_positive() {
+	public void test_extract_strong_positive() {
 		SWN3 myswn = new SWN3();
 		String test = myswn.extract("saintly","a");
 		assertTrue(test == "strong_positive");
 	}
 	@Test
-	public void test_positive() {
+	public void test_extract_positive() {
 		SWN3 myswn = new SWN3();
 		String test = myswn.extract("good","a");
 		assertTrue(test == "positive");
 	}
 	@Test
-	public void test_weak_positive() {
+	public void test_extract_weak_positive() {
 		SWN3 myswn = new SWN3();
 		String test = myswn.extract("due","a");
 		assertTrue(test == "weak_positive");
 	}
 	@Test
-	public void test_Neutral() {
+	public void test_extract_Neutral() {
 		SWN3 myswn = new SWN3();
 		String test = myswn.extract("anadromous","a");
 		assertTrue(test == "neutral");
 	}
 	@Test
-	public void test_weak_negative() {
+	public void test_extract_weak_negative() {
 		SWN3 myswn = new SWN3();
 		String test = myswn.extract("landscape","n");
 		assertTrue(test == "weak_negative");
 	}
 	@Test
-	public void test_negative() {
+	public void test_extract_negative() {
 		SWN3 myswn = new SWN3();
 		String test = myswn.extract("bad","a");
 		assertTrue(test == "negative");
 	}
 	@Test
-	public void test_strong_negative() {
+	public void test_extract_strong_negative() {
 		SWN3 myswn = new SWN3();
 		String test = myswn.extract("terrible","a");
 		assertTrue(test == "strong_negative");
 	}
 	@Test
-	public void test_very_strong_negative() {
+	public void test_extract_very_strong_negative() {
 		SWN3 myswn = new SWN3();
 		String test = myswn.extract("uncool","a");
 		assertTrue(test == "very_strong_negative");
 	}
+	@Test
+	public void test_sentiment_very_strong_positive() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(1.0);
+		assertTrue(test == "very_strong_positive");
+	}
+	@Test
+	public void test_sentiment_strong_positive() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(0.74);
+		assertTrue(test == "strong_positive");
+	}
+	@Test
+	public void test_sentiment_positive() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(0.49);
+		assertTrue(test == "positive");
+	}
+	@Test
+	public void test_sentiment_weak_positive() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(0.24);
+		assertTrue(test == "weak_positive");
+	}
+	@Test
+	public void test_sentiment_Neutral() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(0);
+		assertTrue(test == "neutral");
+	}
+	@Test
+	public void test_sentiment_weak_negative() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(-0.24);
+		assertTrue(test == "weak_negative");
+	}
+	@Test
+	public void test_sentiment_negative() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(-0.49);
+		assertTrue(test == "negative");
+	}
+	@Test
+	public void test_sentiment_strong_negative() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(-0.74);
+		assertTrue(test == "strong_negative");
+	}
+	@Test
+	public void test_sentiment_very_strong_negative() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(-1.0);
+		assertTrue(test == "very_strong_negative");
+	}
+	@Test
+	public void test_sentiment_below_bounds() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(-1.1);
+		assertFalse(test == "very_strong_negative");
+	}
+	@Test
+	public void test_sentiment_above_bounds() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(1.1);
+		assertFalse(test == "very_strong_positive");
+	}
+	@Test
+	public void test_sentiment_not_very_strong_positive() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(0);
+		assertFalse(test == "very_strong_positive");
+	}
+	@Test
+	public void test_sentiment_not_strong_positive() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(0);
+		assertFalse(test == "strong_positive");
+	}
+	@Test
+	public void test_sentiment_not_positive() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(0);
+		assertFalse(test == "positive");
+	}
+	@Test
+	public void test_sentiment_not_weak_positive() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(0);
+		assertFalse(test == "weak_positive");
+	}
+	@Test
+	public void test_sentiment_not_Neutral() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(1.0);
+		assertFalse(test == "neutral");
+	}
+	@Test
+	public void test_sentiment_not_weak_negative() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(0);
+		assertFalse(test == "weak_negative");
+	}
+	@Test
+	public void test_sentiment_not_negative() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(0);
+		assertFalse(test == "negative");
+	}
+	@Test
+	public void test_sentiment_not_strong_negative() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(0);
+		assertFalse(test == "strong_negative");
+	}
+	@Test
+	public void test_sentiment_not_very_strong_negative() {
+		SWN3 myswn = new SWN3();
+		String test = myswn.getSentiment(0);
+		assertFalse(test == "very_strong_negative");
+	}
+	
 }
